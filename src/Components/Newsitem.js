a5e7af05993da16ca0f31ec0a17379123ee285f6
Lakshay Tandon <tandonlakshay6@gmail.com>
2024-07-28 20:39:19 +0530

    first commit

---
+++ src/Components/Newsitem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        this is a new item
+        
+      </div>
+    )
+  }
+}
+
+export default Newsitem
